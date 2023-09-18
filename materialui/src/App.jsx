5c6312fa90b7c6752e8a11d9e9b5fcf598afca60
farooq-teqniqly <farooq@teqniqly.com>
2023-09-18 16:46:48 -0700

    MUI button example

--- materialui/src/App.jsx
+++ materialui/src/App.jsx
@@ -1,5 +1,15 @@
+import Button from "@mui/material/Button";
+
 function App() {
-  return <div></div>;
+  const handleClick = (e) => {
+    console.log(e);
+  };
+
+  return (
+    <Button variant="contained" onClick={handleClick}>
+      First MUI Component
+    </Button>
+  );
 }
 
 export default App;
--- materialui/src/App.test.jsx
+++ materialui/src/App.test.jsx
@@ -1,6 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders without crashing", () => {
-  render(<App />);
+describe("App component", () => {
+  it("has a button", () => {
+    render(<App></App>);
+    const button = screen.getByRole("button", { name: "First MUI Component" });
+    expect(button).toBeInTheDocument();
+  });
 });
